83418971267c774a09c899f463a6a4817004f401
yazem <yassineazem@yahoo.fr>
2021-08-05 10:45:13 +0200

    change to deploy

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home({ results }) {
       <Head>
         <title>Hulu</title>
         <meta name="description" content="Hulu web clone" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/hulu-icon.png" />
       </Head>
 
       <Header />
